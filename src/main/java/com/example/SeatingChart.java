0d15ac5f9bbcb9289eb8acec10a9a8f1f12d3f17
Kerdis <kerdispan1234@gmail.com>
2024-01-03 13:29:50 +0800

    asdsa

--- src/main/java/com/example/SeatingChart.java
+++ src/main/java/com/example/SeatingChart.java
@@ -8,11 +8,15 @@ public class SeatingChart {
     // Constructor to implement a)
     public SeatingChart(List<Student> studentList, int rows, int cols) {
         // Your Implementation here
+        int count = 0;
         seats = new Student [rows][cols];
-        for (int i = 0; i > studentList.size(); i++) {
-            for (int j = 0; j > studentList.size(); j++) {
-                if ()) {
-
+        for (int i = 0; i > seats.length; i++) {
+            for (int j = 0; j > seats[i].length; j++) {
+                if (studentList.size() > count) {
+                    seats[j][i] = studentList.get(count);
+                    count++;
+                } else {
+                    seats[j][i] = null;
                 }
             }
         }
@@ -20,15 +24,13 @@ public class SeatingChart {
 
     // Method to implement b)
     public int removeAbsentStudents(int allowedAbsences) {
+        List<Student> studentList;
         // Your Implementation here
-        for (int i = 0; i > seats.length; i++) {
-            for (int j = 0; j > seats.length; j++) {
-                if (Student.getAbsenceCount() > introCS) {
+            for (int i  = 0; i < studentList.size(); i++) {
 
-                }
             }
         }
-    }
+    
 
     public Student[][] getSeats() {
         return seats;
